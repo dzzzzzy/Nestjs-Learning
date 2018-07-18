df719661d84df90a288857747c30d8d08003ef0c
dzzzzzy <insistyao_0522@163.com>
2018-07-18 14:39:01 +0800

    增加全局异常拦截器

--- demo/zoo/src/application.module.ts
+++ demo/zoo/src/application.module.ts
@@ -1,5 +1,9 @@
 import { Module } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AnimalModule } from "animals/animal.module";
 import { ApplicationService } from "application.service";
+import { ErrorsInterceptor } from "common/errors.interceptor";
 
 import { ApplicationController } from "./application.controller";
 
@@ -7,9 +11,15 @@ import { ApplicationController } from "./application.controller";
  * @Module() 定义一个模块，并管理这个模块的导入集合、控制器集合、提供者集合、导出集合
  */
 @Module({
-    imports: [],  // 导入其他模块的集合
+    imports: [TypeOrmModule.forRoot(), AnimalModule],  // 导入其他模块的集合
     controllers: [ApplicationController],  // 当前模块的控制器集合
-    providers: [ApplicationService],  // 当前模块的提供者集合
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ErrorsInterceptor
+        },
+        ApplicationService
+    ],  // 当前模块的提供者集合
     exports: [], // 导出当前模块的提供者，用于被其他模块调用
 })
 export class ApplicationModule { }
--- demo/zoo/src/server.ts
+++ demo/zoo/src/server.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { ApplicationModule } from "application.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(ApplicationModule);  // 创建应用程序实例
+    const app = await NestFactory.create(ApplicationModule);  // 创建应用程序实例，此时所有被 ApplicationModule 导入的其他模块的所有实例都会被加载
     await app.listen(3000);  // 使用3000端口监听应用程序
 }
 
