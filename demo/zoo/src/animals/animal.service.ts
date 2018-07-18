8b2351fe6cbc6fed279c5f1ea09b982062b2c205
dzzzzzy <insistyao_0522@163.com>
2018-07-18 16:46:44 +0800

    更新代码逻辑

--- demo/zoo/src/animals/animal.service.ts
+++ demo/zoo/src/animals/animal.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
@@ -22,8 +22,16 @@ export class AnimalService {
         // this.animalRepo.create(animal);
 
         /**
-         * 将给定实体保存在数据库中。如果数据库中不存在实体，则插入，否则更新。
+         * 先根据 name huo alias 查询一个记录，如果存在则不进行插入
+         *
+         * 如果不存在，则将给定实体保存在数据库中
          */
+        const exist = await this.animalRepo.createQueryBuilder("animal").where("animal.name = :name", { name: animal.name }).orWhere("animal.alias = :alias", { alias: animal.alias }).getOne();
+        if (exist) {
+            throw new HttpException("该动物已被创建", 409);
+        }
+        // 插入数据时，删除 id，以避免请求体内传入 id
+        delete animal.id;
         return this.animalRepo.save(animal);
 
         /**
@@ -39,6 +47,7 @@ export class AnimalService {
      * @param id 动物ID
      */
     async deleteAnimal(id: number): Promise<void> {
+        await this.findOneById(id);
         this.animalRepo.delete(id);
     }
 
@@ -48,6 +57,8 @@ export class AnimalService {
      * @param animal 动物实体对象
      */
     async updateAnimal(id: number, animal: Animal): Promise<void> {
+        await this.findOneById(id);
+        // 更新数据时，删除 id，以避免请求体内传入 id
         delete animal.id;
         this.animalRepo.update(id, animal);
     }
@@ -58,6 +69,18 @@ export class AnimalService {
      * @param id 动物ID
      */
     async findOneAnimal(id: number): Promise<Animal> {
-        return this.animalRepo.findOne(id);
+        return this.findOneById(id);
+    }
+
+    /**
+     * 根据ID查询单个动物信息，如果不存在则抛出404异常
+     * @param id 动物ID
+     */
+    private async findOneById(id: number): Promise<Animal> {
+        const animalInfo = await this.animalRepo.findOne(id);
+        if (!animalInfo) {
+            throw new HttpException(`指定 id=${id} 不存在`, 404);
+        }
+        return animalInfo;
     }
 }
