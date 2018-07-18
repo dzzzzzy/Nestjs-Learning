5ecf5d7a5491242ff92e6acc1e4a16821360b28c
dzzzzzy <insistyao_0522@163.com>
2018-07-18 16:17:36 +0800

    修复路由参数bug

--- demo/zoo/src/animals/animal.controller.ts
+++ demo/zoo/src/animals/animal.controller.ts
@@ -17,19 +17,19 @@ export class AnimalController {
     }
 
     @Delete(":id")
-    async deleteAnimal(@Param() id: number): Promise<Result> {
+    async deleteAnimal(@Param("id") id: number): Promise<Result> {
         await this.animalService.deleteAnimal(id);
         return { code: 200, message: "删除动物成功" };
     }
 
     @Put(":id")
-    async updateAnimal(@Param() id: number, @Body() animal: Animal): Promise<Result> {
+    async updateAnimal(@Param("id") id: number, @Body() animal: Animal): Promise<Result> {
         await this.animalService.updateAnimal(id, animal);
         return { code: 200, message: "更新动物成功" };
     }
 
     @Get(":id")
-    async findOneAnimal(@Param() id: number): Promise<Result> {
+    async findOneAnimal(@Param("id") id: number): Promise<Result> {
         const data = await this.animalService.findOneAnimal(id);
         return { code: 200, message: "查询动物成功", data };
     }
