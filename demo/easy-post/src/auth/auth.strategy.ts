e56406627df6a888b20c5a9de6720f3df7169afa
dzzzzzy <insistyao_0522@163.com>
2018-09-29 11:56:55 +0800

    增加注释

--- demo/easy-post/src/auth/auth.strategy.ts
+++ demo/easy-post/src/auth/auth.strategy.ts
@@ -6,6 +6,10 @@ import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthStrategy extends PassportStrategy(Strategy) {
+    /**
+     * 这里的构造函数向父类传递了授权时必要的参数，在实例化时，父类会得知授权时，客户端的请求必须使用 Authorization 作为请求头，
+     * 而这个请求头的内容前缀也必须为 Bearer，在解码授权令牌时，使用秘钥 secretOrKey: 'secretKey' 来将授权令牌解码为创建令牌时的 payload。
+     */
     constructor(private readonly authService: AuthService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -13,6 +17,12 @@ export class AuthStrategy extends PassportStrategy(Strategy) {
         });
     }
 
+    /**
+     * validate 方法实现了父类的抽象方法，在解密授权令牌成功后，即本次请求的授权令牌是没有过期的，
+     * 此时会将解密后的 payload 作为参数传递给 validate 方法，这个方法需要做具体的授权逻辑，比如这里我使用了通过用户名查找用户是否存在。
+     * 当用户不存在时，说明令牌有误，可能是被伪造了，此时需抛出 UnauthorizedException 未授权异常。
+     * 当用户存在时，会将 user 对象添加到 req 中，在之后的 req 对象中，可以使用 req.user 获取当前登录用户。
+     */
     async validate(payload: { account: string }) {
         const user = await this.authService.validateUser(payload);
         if (!user) {
--- demo/easy-post/src/controllers/user.controller.ts
+++ demo/easy-post/src/controllers/user.controller.ts
@@ -12,6 +12,11 @@ export class UserController {
         @Inject(UserService) private readonly userService: UserService
     ) { }
 
+    /**
+     * 用户登录成功后，返回的 data 是授权令牌；
+     * 在调用有 @UseGuards(AuthGuard()) 注解的路由时，会检查当前请求头中是否包含 Authorization: Bearer xxx 授权令牌，
+     * 其中 Authorization 是用于告诉服务端本次请求有令牌，并且令牌前缀是 Bearer，而令牌的具体内容是登录之后返回的 data(accessToken)。
+     */
     @Post('login')
     async login(@Body() body: { account: string, password: string }): Promise<Result> {
         await this.userService.login(body.account, body.password);
