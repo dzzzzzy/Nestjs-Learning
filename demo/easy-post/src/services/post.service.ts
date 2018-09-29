7a80c5a3dd4be46b6e31c274b790991dae722f67
dzzzzzy <insistyao_0522@163.com>
2018-09-29 11:57:10 +0800

    增加增删改查逻辑

--- demo/easy-post/src/services/post.service.ts
+++ demo/easy-post/src/services/post.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -9,6 +9,26 @@ export class PostService {
     constructor(
         @InjectRepository(Post) private readonly postRepo: Repository<Post>
     ) { }
+
+    async create(post: Post): Promise<void> {
+        await this.postRepo.save(post);
+    }
+
+    async remove(id: number): Promise<void> {
+        const existing = await this.findOneById(id);
+        if (!existing) throw new HttpException(`删除失败，ID 为 '${id}' 的帖子不存在`, 404);
+    }
+
+    async update(id: number, title?: string, content?: string): Promise<void> {
+        const existing = await this.findOneById(id);
+        if (!existing) throw new HttpException(`更新失败，ID 为 '${id}' 的帖子不存在`, 404);
+        if (title) existing.title = title ? title : existing.title;
+        if (content) existing.content = content ? content : existing.content;
+    }
+
+    async findOneById(id: number): Promise<Post> {
+        return await this.postRepo.findOne(id);
+    }
 
     async findAll(): Promise<Post[]> {
         return await this.postRepo.find();
