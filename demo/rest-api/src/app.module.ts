1cd5b8be76863f9dc1a92f6c315ca5f3f35becef
dzzzzzy <insistyao_0522@163.com>
2018-11-23 16:36:10 +0800

    fix rest-api demo bug

--- demo/rest-api/src/app.module.ts
+++ demo/rest-api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
@@ -12,7 +11,7 @@ import { ErrorsInterceptor } from './common/errors.interceptor';
  * @Module() 定义一个模块，并管理这个模块的导入集合、控制器集合、提供者集合、导出集合
  */
 @Module({
-    imports: [GraphQLModule, TypeOrmModule.forRoot(), CatModule],  // 导入其他模块的集合
+    imports: [TypeOrmModule.forRoot(), CatModule],  // 导入其他模块的集合
     controllers: [AppController],  // 当前模块的控制器集合
     providers: [
         {
